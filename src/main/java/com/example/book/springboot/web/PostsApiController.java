f7c548d1cbea503f204210e63953f54eb15c9afc
cjy71 <cjy71092298@gmail.com>
2021-07-11 21:54:47 +0900

    update board pages and test codes

---
+++ src/main/java/com/example/book/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.example.book.springboot.web;
+
+import com.example.book.springboot.service.PostsService;
+import com.example.book.springboot.web.dto.PostUpdateRequestDto;
+import com.example.book.springboot.web.dto.PostsResponseDto;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
---
+++ src/test/java/com/example/book/springboot/HelloControllerTest.java
@@ -0,0 +1,67 @@
+package com.example.book.springboot;
+
+import com.example.book.springboot.web.HelloController;
+import com.example.book.springboot.web.HelloResponseDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void test () {
+
+    }
+
+    @Test
+    public void lombokTest(){
+        String name = "test";
+        int amount=1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+
+        String name ="hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                    .param("name",name)
+                    .param("amount",String.valueOf(amount)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name",is(name)))
+                    .andExpect(jsonPath("$.amount",is(amount)));
+    }
+
+
+
+    @Test
+    public void hello() throws Exception{
+        String hello= "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
