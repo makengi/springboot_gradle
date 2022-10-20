0d24b4b58f1229531cf38952c8f77b817324f5d5
최지영 <cjy@imrbiz.co.kr>
2022-10-20 09:22:33 +0900

    update: add files

---
+++ src/test/java/com/example/book/springboot/EtcTest.java
@@ -0,0 +1,20 @@
+package com.example.book.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+public class EtcTest {
+
+    @Test
+    public void algorithm(){
+
+    }
+}
